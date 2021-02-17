fe1011bb231b19ccfdf731c80ecca0b1f1aa42fa
Lincoln Shade <lincoln.shade@uky.edu>
2021-02-17 15:47:44 +0000

    updates

--- code/single_variant_analysis/02_clump_top_snps.sh
+++ code/single_variant_analysis/02_clump_top_snps.sh
@@ -1,4 +1,7 @@
 #!/bin/bash 
 
 # nacc/adgc unrelated
-bash code/single_variant_analysis/clump_top_snps.sh data/nacc_adgc/nacc_adgc_unrelated output/nacc_adgc/nacc_adgc_unrelated.assoc.logistic "output/nacc_adgc/nacc_adgc_unrelated"
+bash code/single_variant_analysis/clump_top_snps.sh \
+  data/nacc_adgc/nacc_adgc_unrelated \
+  output/nacc_adgc/nacc_adgc_unrelated.assoc.logistic \
+  "output/nacc_adgc/nacc_adgc_unrelated"
