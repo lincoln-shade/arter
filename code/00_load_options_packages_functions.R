615486ab9fecdcdaf944f0ce28fc86c5f9c7f174
Lincoln Shade <lincoln.shade@uky.edu>
2021-03-05 01:53:49 +0000

    updated manuscript

--- code/00_load_options_packages_functions.R
+++ code/00_load_options_packages_functions.R
@@ -4,7 +4,7 @@
 options(digits = 15)
 
 library(pacman)
-p_load(data.table, magrittr, ggplot2, stringi, readxl, flextable, devtools, qusage)
+p_load(data.table, magrittr, ggplot2, stringi, readxl, flextable, devtools, qusage, ggrepel, knitr)
 # options(kableExtra.auto_format = FALSE)
 
 strip_alleles <- function(x) {
@@ -13,3 +13,76 @@ strip_alleles <- function(x) {
 
 source_url("https://raw.githubusercontent.com/lincoln-shade/r.functions/master/make_table_one.R", 
            sha1 = "618b76ddf7b93f365d7cd45fdf00910011dabb0c")
+
+make_or_95_ci <- function(or, l95, u95, round_digits=2, flip_less_than_1=TRUE) {
+  if (flip_less_than_1) {
+    output <- ifelse(or >= 1,
+                     paste0(round(or, round_digits), " [", round(l95, round_digits), "-", round(u95, round_digits), "]"),
+                     ifelse((or < 1) & (or > 0),
+                            paste0(round(1 / or, round_digits), " [", round(1 / u95, round_digits), "-", round(1 / l95, round_digits), "]"),
+                            paste0("-"))
+    )
+  }
+  
+  output <- ifelse(or > 0,
+                   paste0(round(or, round_digits), " [", round(l95, round_digits), "-", round(u95, round_digits), "]"),
+                   paste0("-"))
+  output
+}
+
+# takes as input a data.table object read from .assoc.logistic file
+# with same column names
+plot_manhattan <- function(results, signif_only=TRUE, annotate=FALSE) {
+  results <- copy(results)
+  results[, BP := as.numeric(BP)]
+  if (signif_only) {
+    results <- results[P < 0.05]
+  }
+  
+  if (length(annotate) > 0) {
+    results[SNP %in% annotate, annotate := TRUE]
+  }
+  
+  # cumulative bp length for each chromosome
+  chr_bp <- results[, max(BP), CHR] %>% 
+    setnames("V1", "chr_length") %>% 
+    # add 20mil buffer to help with spacing of axis labels
+    .[, cum_length := cumsum(chr_length) - chr_length + 20000000*(CHR - 1)]
+  
+  results <- merge(results, chr_bp[, -c("chr_length")], "CHR") %>% 
+    setorder(CHR, BP) %>% 
+    .[, bp_cum := BP + cum_length]
+  
+  x_axis <- results[, (max(bp_cum) + min(bp_cum)) / 2, CHR]
+
+  manplot <- results[, ggplot(.SD, aes(bp_cum, -log10(P)))] +
+    geom_point(aes(color=as.factor(CHR)), alpha=0.8, size=1.3) +
+    scale_color_manual(values = rep(c("#606060", "#000066"), 22)) +
+    geom_hline(yintercept = 7.3, color = "black") + #genome-wide significance threshold
+    geom_hline(yintercept = 5, color = "black") + #suggestibility threshold
+    
+    scale_x_continuous(label = x_axis$CHR, breaks= x_axis$V1) +
+    # remove space between y-axis and baseline
+    scale_y_continuous(expand = c(0.05, 0)) +
+
+    xlab("Chromosome") +
+    ylab("-log(P)") +
+    # Add highlighted points
+    #geom_point(data=subset(snp_mod, is_highlight=="yes"), color="orange", size=2) +
+
+    # Add label using ggrepel to avoid overlapping
+    geom_label_repel(data=results[annotate == TRUE], aes(label=SNP), size=5) +
+
+    # Custom theme:
+    theme_bw() +
+    theme(
+      # text = element_text(family = "Calibri", size = 20),
+      axis.text = element_text(size = 12),
+      legend.position="none",
+      panel.border = element_blank(),
+      panel.grid.major.x = element_blank(),
+      panel.grid.minor.x = element_blank()
+    )
+  
+  manplot
+}
--- code/single_variant_analysis/03_act__nacc_adgc_rosmap_unrelated_meta.sh
+++ code/single_variant_analysis/03_act__nacc_adgc_rosmap_unrelated_meta.sh
@@ -7,4 +7,5 @@
 plink \
   --meta-analysis output/adni_npc/nacc_adgc_rosmap_adni_unrelated.assoc.logistic \
     raw_data/brainartbin.assoc.logistic.txt \
+  --ci 0.95 \
   --out output/act/act__nacc_adgc_rosmap_adni_unrelated
